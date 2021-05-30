671611dca0b5328e108a9ddb2e6380ef85815254
lucasbaquinoo <lucasbaquino@icloud.com>
2021-05-30 19:58:32 -0300

    create the query and moderation service

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const axios = require('axios');
 
-const app = express().json();
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 const commentsByPostId = {};
@@ -12,13 +14,13 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: 'pending' });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -27,12 +29,31 @@ app.post('/posts/:id/comments', (req, res) => {
     data: { 
       id: commentId,
       commentId,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   })
 
   res.status(201).send(comments);
 });
+
+app.post('/events', (req, res) => {
+  console.log('Event Received:', req.body.type);
+
+  const { type, data } = req.body
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status } = data
+    const comments = commentsByPostId[postId]
+
+    const comment = comments.find(comment => {
+      return comment.id === id
+    })
+    comment.status = status
+  }
+
+  res.send({})
+})
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
