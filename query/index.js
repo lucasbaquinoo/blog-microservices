55a9d01f84749c264ab1d63df13eb3d18ef4d6bc
lucasbaquinoo <lucasbaquino@icloud.com>
2021-05-30 21:37:13 -0300

    finished example

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const cors = require('cors')
 
 const app = express()
@@ -8,23 +9,7 @@ app.use(cors())
 
 const posts = {}
 
-posts === {
-  'j123j42': {
-    id: 'j123j42',
-    title: 'post title',
-    comments: [
-      { id: 'klj3kl', content: 'comment!' }
-    ]
-  }
-}
-
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
-
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data
 
@@ -47,10 +32,31 @@ app.post('/events', (req, res) => {
     comment.status = status
     comment.content = content
   }
+}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body
+
+  handleEvent(type, data)
 
   res.send({})
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002')
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+ 
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+ 
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 })
